043527926e245ba90b2f2bb82288bf6885ad73bc
Dev-Waqas232 <devwaqas232@gmail.com>
2025-05-30 12:56:48 +0500

    Commented Hello World Program

--- cpp_basics/hello_world.cpp
+++ cpp_basics/hello_world.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  // standard stream I/O facilities are conatined in iostream
+using namespace std; // makes name from std visible without using std:: infront of every name
 
 int main()
 {
-    cout << "Hello World!";
+    // cout is the name defined in the std namespace
+    cout << "Hello World!"; // << operator means 'put to'. It writes the second argument into the first one, in this case our string hello world is being put in to the standard output stream
 }
